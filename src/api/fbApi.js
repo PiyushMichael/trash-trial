aa8ad9bdeb1d7e636f8cd875027ae3eb3f62b336
Michael Augustine <piyush.michael93@gmail.com>
2019-12-14 20:39:46 +0530

    FBSDK started

---
+++ src/api/fbApi.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const appId = '700637940423400';
+const appSecret = 'bda5db70b25acc3cf0a503bd8934e590';
+
+
+// export const sampleApi = () => {
+//     return fb.api('4');
+// }
--- src/components/fb.js
+++ src/components/fb.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { GraphRequest, GraphRequestManager, LoginButton, AccessToken } from 'react-native-fbsdk';
 
 class Fb extends Component {
+    componentDidMount() {
+        this.testRequestGraphAPI();
+    }
+
+    _responseInfoCallback(error: ?Object, result: ?Object) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(result);
+        }
+      }
+    
+    testRequestGraphAPI(){
+        const infoRequest = new GraphRequest(
+        '/me',
+        null,
+        this._responseInfoCallback,
+        );   
+        new GraphRequestManager().addRequest(infoRequest).start();
+      }
+
+    displayToken() {
+      AccessToken.getCurrentAccessToken()
+      .then(data => {
+          console.log(data);
+        }
+      )
+      .catch();
+    }
+
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center' }}>
                 <Text>Facebook</Text>
+                <LoginButton
+                  publishPermissions={["email"]}
+                  onLoginFinished={(error, result) => {
+                    console.log('HIT IT');
+                      if (error) {
+                        console.log("login has error: " + result.error);
+                      } else if (result.isCancelled) {
+                        console.log("login is cancelled.");
+                      } else {
+                        AccessToken.getCurrentAccessToken().then(
+                          (data) => {
+                            console.log(data.accessToken.toString())
+                          }
+                        )
+                      }
+                    }
+                  }
+                  onLogoutFinished={() => console.log("logout.")}
+                />
+                <Button title="Access Token" onPress={() => this.displayToken()} />
             </View>
         );
     }
