d0401ec59f64046094b6ee358783972ffe38946a
Michael Augustine <piyush.michael93@gmail.com>
2019-12-15 11:39:32 +0530

    FB ID worked somewhat

--- src/api/fbApi.js
+++ src/api/fbApi.js
@@ -3,7 +3,25 @@ import axios from 'axios';
 const appId = '700637940423400';
 const appSecret = 'bda5db70b25acc3cf0a503bd8934e590';
 
-
-// export const sampleApi = () => {
-//     return fb.api('4');
-// }
+export const getFBidApi = () => {
+    const url = 'https://www.facebook.com:443/piyush.michael';
+    const axiosInstance = axios.create({
+        headers: {
+            'accept': 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
+            'Host': 'www.facebook.com',
+            'Connection': 'Keep-Alive',
+            'Accept-Language': 'en-GB,en-US;q=0.8,en;q=0.6'
+        }
+    });
+    return new Promise((resolve, reject) => {
+        axiosInstance.get(url)
+        .then(res => {
+            const id = res.data.match(/"entity_id":"(\d*)"/);
+            resolve(id);
+        })
+        .catch(e => {
+            reject(e)
+        });
+    });
+}
--- src/components/fb.js
+++ src/components/fb.js
@@ -1,62 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
-import { GraphRequest, GraphRequestManager, LoginButton, AccessToken } from 'react-native-fbsdk';
+import { getFBidApi } from 'api/fbApi';
 
 class Fb extends Component {
     componentDidMount() {
-        this.testRequestGraphAPI();
-    }
-
-    _responseInfoCallback(error: ?Object, result: ?Object) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(result);
-        }
-      }
-    
-    testRequestGraphAPI(){
-        const infoRequest = new GraphRequest(
-        '/me',
-        null,
-        this._responseInfoCallback,
-        );   
-        new GraphRequestManager().addRequest(infoRequest).start();
-      }
-
-    displayToken() {
-      AccessToken.getCurrentAccessToken()
-      .then(data => {
-          console.log(data);
-        }
-      )
-      .catch();
+      getFBidApi()
+      .then(res => {
+        console.log(res);
+      })
+      .catch(e => {
+        console.log(e)
+      });
     }
 
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center' }}>
                 <Text>Facebook</Text>
-                <LoginButton
-                  publishPermissions={["email"]}
-                  onLoginFinished={(error, result) => {
-                    console.log('HIT IT');
-                      if (error) {
-                        console.log("login has error: " + result.error);
-                      } else if (result.isCancelled) {
-                        console.log("login is cancelled.");
-                      } else {
-                        AccessToken.getCurrentAccessToken().then(
-                          (data) => {
-                            console.log(data.accessToken.toString())
-                          }
-                        )
-                      }
-                    }
-                  }
-                  onLogoutFinished={() => console.log("logout.")}
-                />
-                <Button title="Access Token" onPress={() => this.displayToken()} />
+                
+                <Button title="Access Token" />
             </View>
         );
     }
